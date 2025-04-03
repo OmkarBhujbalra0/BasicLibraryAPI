8a7e4850b4bfa4d14a7ee05b6cff1ea4d44aacd8
omkar <omkarbhujbalrao6@gmail.com>
2025-04-04 02:17:57 +0530

    Fixes

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ with app.app_context():
 # Run the app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port,debug=True)
